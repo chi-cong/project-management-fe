128b8275d4ee13432e037e015ccc49f641155b4c
Vũ Trọng Đạt <81927305+datvuhp2002@users.noreply.github.com>
2024-08-06 10:09:15 +0700

    projects department

---
+++ src/pages/departments/departments.tsx
@@ -0,0 +1,9 @@
+import { CardDepartments } from "src/layouts/card-departments-list/card-departments-list";
+
+export const Departments = () => {
+  return (
+    <>
+      <CardDepartments />
+    </>
+  );
+};
