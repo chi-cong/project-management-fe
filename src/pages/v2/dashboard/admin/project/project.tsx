2c89c6fee094db1eb1274fa4d52410135b1e5794
Chi Cong <chicong422002@gmail.com>
2024-08-06 23:58:06 +0700

    add project detail, update department UI

--- src/pages/v2/dashboard/admin/project/project.tsx
+++ src/pages/v2/dashboard/admin/project/project.tsx
@@ -1,9 +1,41 @@
 import "./project.css";
-import { MenuDots, Pen, Trash, Page, Plus } from "src/assets/icons";
-import { Typography, Button, Avatar, Popconfirm, Popover } from "antd";
-import { CustomAvatar } from "src/components/v2";
+import { MenuDots, Pen, Trash, Page } from "src/assets/icons";
+import {
+  Typography,
+  Button,
+  Avatar,
+  Popconfirm,
+  Popover,
+  List,
+  Modal,
+} from "antd";
+import { PlusOutlined } from "@ant-design/icons";
+import { CustomAvatar, DocumentSection } from "src/components/v2";
+import { TaskList, TaskDetail } from "src/layouts/v2";
+import { useState } from "react";
 
 export const AdminProject = () => {
+  const [taskDetailModal, setTaskDetailModal] = useState<boolean>(false);
+  const [docSec, setDocSec] = useState<boolean>(false);
+
+  const taskListSrc: {
+    color: string;
+    title: string;
+  }[] = [
+    {
+      color: "var(--primary-color)",
+      title: "Todo",
+    },
+    {
+      color: "#FFA500",
+      title: "On progress",
+    },
+    {
+      color: "#8BC48A",
+      title: "Done",
+    },
+  ];
+
   const ProjectOptions = () => {
     return (
       <div className='project-option'>
@@ -26,31 +58,67 @@ export const AdminProject = () => {
   };
 
   return (
-    <div className='admin-project-page'>
-      <header className='header-row'>
-        <div className='first-part'>
-          <Typography.Title level={2}>Mobile App</Typography.Title>
-          <Popover content={ProjectOptions} trigger='click'>
-            <Button type='text' size='small'>
-              <MenuDots />
+    <>
+      <div className='admin-project-page'>
+        <header className='header-row'>
+          <div className='first-part'>
+            <Typography.Title level={2}>Mobile App</Typography.Title>
+            <Popover content={ProjectOptions} trigger='click'>
+              <Button type='text' size='small'>
+                <MenuDots />
+              </Button>
+            </Popover>
+          </div>
+          <div className='second-part'>
+            <Button type='primary' className='create-task-btn'>
+              <PlusOutlined />
+              <Typography.Text style={{ color: "white" }}>
+                Create Task
+              </Typography.Text>
             </Button>
-          </Popover>
-        </div>
-        <div className='second-part'>
-          <Button type='primary' className='create-task-btn'>
-            <Plus />
-            <Typography.Text style={{ color: "white" }}>
-              Create Task
-            </Typography.Text>
-          </Button>
-          <Avatar.Group maxCount={3}>
-            <CustomAvatar size={32} userName='abcd' />
-            <CustomAvatar size={32} userName='bcda' />
-            <CustomAvatar size={32} userName='cdab' />
-            <CustomAvatar size={32} userName='dabc' />
-          </Avatar.Group>
-        </div>
-      </header>
-    </div>
+            <Avatar.Group maxCount={3}>
+              <CustomAvatar size={32} userName='abcd' />
+              <CustomAvatar size={32} userName='bcda' />
+              <CustomAvatar size={32} userName='cdab' />
+              <CustomAvatar size={32} userName='dabc' />
+            </Avatar.Group>
+          </div>
+        </header>
+        <List
+          grid={{
+            gutter: 16,
+            xs: 1,
+            sm: 1,
+            md: 2,
+            lg: 2,
+            xl: 3,
+            xxl: 3,
+          }}
+          className='task-sec'
+          dataSource={taskListSrc}
+          renderItem={(taskList) => {
+            return (
+              <List.Item>
+                <TaskList
+                  color={taskList.color}
+                  title={taskList.title}
+                  showTaskDetail={setTaskDetailModal}
+                  showDocSec={setDocSec}
+                />
+              </List.Item>
+            );
+          }}
+        />
+      </div>
+
+      <TaskDetail
+        open={taskDetailModal}
+        setShowTaskDetail={setTaskDetailModal}
+        project={{}}
+      />
+      <Modal open={docSec} onCancel={() => setDocSec(false)} footer={[]}>
+        <DocumentSection />
+      </Modal>
+    </>
   );
 };
