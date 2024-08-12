b9d545e86e9ec325e02d380ac95007caec9d4cf2
Vũ Trọng Đạt <81927305+datvuhp2002@users.noreply.github.com>
2024-08-12 13:29:53 +0700

    password

---
+++ src/pages/v2/password/password.tsx
@@ -0,0 +1,61 @@
+import { LoginFieldType } from "src/layouts";
+import "./password.css";
+import { Button, Card, Col, Form, Input, Row, Space, Typography } from "antd";
+
+export const Password = () => {
+  return (
+    <div className="v2-password-page">
+      <Row className="content-container">
+        <Space direction="vertical" size="middle" style={{ width: "100%" }}>
+          <div className="title-row">
+            <h2>Change Password</h2>
+          </div>
+          <div className="form-wrapper">
+            <Card>
+              <Form
+                name="password"
+                className="password-form"
+                autoComplete="off"
+              >
+                <Form.Item<LoginFieldType>
+                  name="password"
+                  rules={[{ required: true, message: "Password is required" }]}
+                  style={{ width: "100%" }}
+                >
+                  <Input.Password placeholder="Old Password.." size="large" />
+                </Form.Item>
+                <Form.Item<LoginFieldType>
+                  name="password"
+                  rules={[{ required: true, message: "Password is required" }]}
+                  style={{ width: "100%" }}
+                >
+                  <Input.Password placeholder="New Password..." size="large" />
+                </Form.Item>
+                <Form.Item<LoginFieldType>
+                  name="password"
+                  rules={[{ required: true, message: "Password is required" }]}
+                  style={{ width: "100%" }}
+                >
+                  <Input.Password
+                    placeholder="Confirm new Password...."
+                    size="large"
+                  />
+                </Form.Item>
+                <Form.Item>
+                  <Button
+                    className="password-button"
+                    type="primary"
+                    htmlType="submit"
+                    size="large"
+                  >
+                    Change
+                  </Button>
+                </Form.Item>
+              </Form>
+            </Card>
+          </div>
+        </Space>
+      </Row>
+    </div>
+  );
+};
