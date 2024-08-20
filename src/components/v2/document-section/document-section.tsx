315c31de73e77f9a5b5ca186f1af4162be2a74ff
chicong <chicong422002@gamil.com>
2024-08-20 10:33:41 +0700

    delete console log

--- src/components/v2/document-section/document-section.tsx
+++ src/components/v2/document-section/document-section.tsx
@@ -77,7 +77,6 @@ export const DocumentSection = () => {
   useEffect(() => {
     setFileLinks([]);
     const tempFileLinks: string[] = [];
-    console.log(task);
     task?.document.map((file) =>
       getFile({ file })
         .unwrap()
@@ -91,29 +90,29 @@ export const DocumentSection = () => {
   }, [getFile, task, taskAssignment]);
 
   return (
-    <div className="doc-sec">
-      <div className="doc-sec-first-part">
-        <div className="doc-sec-head">
+    <div className='doc-sec'>
+      <div className='doc-sec-first-part'>
+        <div className='doc-sec-head'>
           <Typography.Title level={4}>File Attachment</Typography.Title>
         </div>
 
-        <div className="file-list">
+        <div className='file-list'>
           {fileLinks.map((files, index) => {
             const fileName = files.split("/").pop();
             const handledFile = handleFile(files);
             return (
-              <div className="file-row" key={index}>
-                <div className="file-name-icon">
+              <div className='file-row' key={index}>
+                <div className='file-name-icon'>
                   {handledFile.fileIcon}
                   <Typography.Link>
-                    <a href={files} target="_blank">
+                    <a href={files} target='_blank'>
                       {fileName}
                     </a>
                   </Typography.Link>
                 </div>
                 {!checkRole(OUserRole.Staff) && (
                   <Popconfirm
-                    title="Delete this file ?"
+                    title='Delete this file ?'
                     onConfirm={() => {
                       // fileLinks and filenames index are the same
                       deleteFile({
@@ -132,7 +131,7 @@ export const DocumentSection = () => {
                         });
                     }}
                   >
-                    <Button shape="round" danger size="small">
+                    <Button shape='round' danger size='small'>
                       Delete
                     </Button>
                   </Popconfirm>
@@ -145,7 +144,7 @@ export const DocumentSection = () => {
       {!checkRole(OUserRole.Staff) ||
         (checkRole(OUserRole.Staff) &&
           taskAssignment?.assignment?.user_id === user?.user_id && (
-            <Upload.Dragger {...props} listType="text">
+            <Upload.Dragger {...props} listType='text'>
               <strong>Choose a file</strong> or drag it here
             </Upload.Dragger>
           ))}
