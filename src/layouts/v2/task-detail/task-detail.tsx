739a7bfd16a57ca054751c2f6fdc544c184f3344
DuonggPh <104338962+DuonggPh@users.noreply.github.com>
2024-09-17 10:13:49 +0700

    TEST_ROUTE

--- src/layouts/v2/task-detail/task-detail.tsx
+++ src/layouts/v2/task-detail/task-detail.tsx
@@ -276,6 +276,7 @@ export const TaskDetail = ({
         isModalOpen={editModal}
         setIsModalOpen={setEditModal}
         project={project}
+        assignment={assignment}
       />
     </Modal>
   );
